f9b9e588bea796d25a4cf23b56896d315608b59f
pranav2305 <pranav2305@gmail.com>
2021-11-17 13:17:17 +0530

    Added ability to give definitions and examples for multiple words

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import os
 
 import discord
+import requests
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+DICT_API = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 
 client = discord.Client()
 
@@ -12,5 +15,32 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content
+
+    if msg.startswith("/dict"):
+        l = msg.split()
+        if not l[1]:
+            await message.channel.send("Please enter atleast 1 word after '\dict'")
+        else:
+            for word in l[1:]:
+                res = requests.get(DICT_API + word)
+                res_json = json.loads(res.text)
+                dict_msg = f"**Word\t:\t{word.lower().capitalize()}**\n\n"
+                for index, meaning in enumerate(res_json[0]["meanings"]):
+                    dict_msg += \
+                        f"{index+1}. ***Part of Speech*** : {meaning['partOfSpeech']}\n"
+                    dict_msg += \
+                        f"    ***Definition*** : {meaning['definitions'][0]['definition']}\n"
+                    dict_msg += \
+                        f"    ***Example*** : {meaning['definitions'][0]['example'].replace('hello', '__hello__')}\n\n"
+
+                await message.channel.send(dict_msg)
 
 client.run(TOKEN)
