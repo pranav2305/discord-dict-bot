38f6cd137eeceffd14e655ab5db257777259f2a9
pranav2305 <pranav2305@gmail.com>
2021-11-18 14:24:03 +0530

    Improved functionality and added new features
    - Added an option to get meaning of slangs
    - Displaying more meanings for "/mean command"
    - Resolved error when there are no phonetics and origin
    - Resolved error while calling getData function

--- README.md
+++ README.md
@@ -6,40 +6,68 @@
 
 ## About
 
-A discord dictionary bot that gives the part of speech, definition, examples, origin, phonetics, synonyms and antonyms. This bot can take multiple inputs and gives the output for all the words after the command in a single message. A flask server is created to upload the discord bot on Repl.it and an uptime bot is used to ping the server every 5 minutes to ensure that the server does'nt go to sleep and the discord bot can be used anytime. The bot also checks if the message is more than 2000 (discord text limit) and splits the message and sends it accordingly.
+A discord dictionary bot that gives the part of speech, definition, examples, origin, phonetics, synonyms and antonyms. This bot can take multiple inputs and gives the output for all the words after the command in a single message. A flask server is created to upload the discord bot on Repl.it and an uptime bot is used to ping the server every 5 minutes to ensure that the server does'nt go to sleep and the discord bot can be used anytime. The bot also checks if the message is more than 2000 (discord text limit) and splits the message and sends it accordingly. The bot shows the Menu when it is added to a new guild.
 <br>
 
 ## Commands
 
-| Command                       | Function                                                       |
-| ----------------------------- | -------------------------------------------------------------- |
-| **/mean** <word1> <word2> ... | Gives the _part of speech, definition and examples._           |
-| **/org** <word1> <word2> ...  | Gives the _origin_ of the words.                               |
-| **/phon** <word1> <word2> ... | Gives the _phonetics text and audio_.                          |
-| **/syn** <word1> <word2> ...  | Gives the _part of speech and the synonyms_ under them.        |
-| **/ant** <word1> <word2> ...  | Gives the _part of speech and the antonyms_ under them.        |
-| **/dict** <word1> <word2> ... | Gives the _meaning, origin, phonetics, synonyms and antonyms_. |
+| Command                       | Function                                                             |
+| ----------------------------- | -------------------------------------------------------------------- |
+| **/mean** <word1> <word2> ... | Gives the _part of speech, definition and examples._                 |
+| **/org** <word1> <word2> ...  | Gives the _origin_ of the words.                                     |
+| **/phon** <word1> <word2> ... | Gives the _phonetics text and audio_.                                |
+| **/syn** <word1> <word2> ...  | Gives the _part of speech and the synonyms_ under them.              |
+| **/ant** <word1> <word2> ...  | Gives the _part of speech and the antonyms_ under them.              |
+| **/dict** <word1> <word2> ... | Gives the _short meaning, origin, phonetics, synonyms and antonyms_. |
+| **/menu**                     | Gives the menu for the dictionary bot                                |
+| **/slang**                    | Gives the _meaning and example_ for slangs.                          |
 
 <br>
 
 ## Samples
 
 1. Command : **/mean hello world**
+   <br>
    ![Output for "/mean hello world"](./images/1.png)
-   <br>
+   <br><br>
 2. Command : **/org helLo WorLd**
+   <br>
    ![Output for "/mean hello world"](./images/2.png)
-   <br>
+   <br><br>
 3. Command : **/phon i like cars**
+   <br>
    ![Output for "/mean hello world"](./images/3.png)
-   <br>
+   <br><br>
 4. Command : **/syn adfe walk rat**
+   <br>
    ![Output for "/mean hello world"](./images/4.png)
-   <br>
+   <br><br>
 5. Command : **/ant happy feet**
-   ![Output for "/mean hello world"](./images/5.png)
    <br>
+   ![Output for "/mean hello world"](./images/5.png)
+   <br><br>
 6. Command : **/dict riddle me this**
+   <br>
    ![Output for "/mean hello world"](./images/6_1.png)
+   <br>
    ![Output for "/mean hello world"](./images/6_2.png)
+   <br>
    ![Output for "/mean hello world"](./images/6_3.png)
+   <br>
+   <br>
+
+7. Command : **/slang wbu smh macha**
+   <br>
+   ![Output for "/slang wbu smh macha"](./images/7.png)
+   <br>
+   <br>
+
+## How to use
+
+- Click on the link provided below and login wih your discord account.
+- Select the server/guild you want to add the bot to.
+- Click on authorize to authorize the bot
+- The bot will send the menu to the first text channel it is authorized for.
+- The bot is ready for use.
+  <br>
+  **Link** : https://bit.ly/3nqGiPW
--- bot.py
+++ bot.py
@@ -7,8 +7,9 @@ from dotenv import load_dotenv
 from server import server
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.environ['DISCORD_TOKEN']
 DICT_API = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+SLANG_API = "https://api.urbandictionary.com/v0/define?term="
 
 client = discord.Client()
 
@@ -17,20 +18,49 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+# Function to get short meanings from the json object
+
+
+def getShortMeaning(res_json):
+    new_msg = ""
+    pos = []
+    index = 1
+    for res_j in res_json:
+        for meaning in res_j["meanings"]:
+            if meaning["partOfSpeech"] not in pos:
+                new_msg += \
+                    f"{index}. ***Part of Speech*** : {meaning['partOfSpeech']}\n"
+                index += 1
+                pos.append(meaning['partOfSpeech'])
+                new_msg += \
+                    f"    ***Definition*** : {meaning['definitions'][0]['definition']}\n"
+                replaceText = f"__*{res_json[0]['word']}*__"
+                if "example" in meaning['definitions'][0]:
+                    new_msg += \
+                        f"    ***Example*** : {meaning['definitions'][0]['example'].replace(res_json[0]['word'], replaceText)}\n\n"
+    return new_msg
+
 # Function to get all the meanings from the json object
 
 
-def getMeaning(res_json):
+def getLongMeaning(res_json):
     new_msg = ""
-    for index, meaning in enumerate(res_json[0]["meanings"]):
-        new_msg += \
-            f"{index+1}. ***Part of Speech*** : {meaning['partOfSpeech']}\n"
-        new_msg += \
-            f"    ***Definition*** : {meaning['definitions'][0]['definition']}\n"
-        replaceText = f"__*{res_json[0]['word']}*__"
-        if "example" in meaning['definitions'][0]:
-            new_msg += \
-                f"    ***Example*** : {meaning['definitions'][0]['example'].replace(res_json[0]['word'], replaceText)}\n\n"
+    pos = []
+    index = 1
+    for res_j in res_json:
+        for meaning in res_j["meanings"]:
+            if meaning["partOfSpeech"] not in pos:
+                new_msg += \
+                    f"{index}. ***Part of Speech*** : {meaning['partOfSpeech']}\n"
+                index += 1
+                pos.append(meaning['partOfSpeech'])
+                for i, defn in enumerate(meaning['definitions']):
+                    new_msg += \
+                        f"    ({chr(ord('a') + i)}.) ***Definition*** : {defn['definition']}\n"
+                    replaceText = f"__*{res_json[0]['word']}*__"
+                    if "example" in meaning['definitions'][0]:
+                        new_msg += \
+                            f"          ***Example*** : {meaning['definitions'][0]['example'].replace(res_json[0]['word'], replaceText)}\n\n"
     return new_msg
 
 # Function to get phonetics from the json object
@@ -38,8 +68,11 @@ def getMeaning(res_json):
 
 def getPhonetics(res_json):
     new_msg = ""
-    new_msg += f"***Text*** : {res_json[0]['phonetic']}\n"
-    new_msg += f"***Audio*** : {res_json[0]['phonetics'][0]['audio']}\n\n"
+    if "phonetic" in res_json[0]:
+        new_msg += f"***Text*** : {res_json[0]['phonetic']}\n"
+        new_msg += f"***Audio*** : {res_json[0]['phonetics'][0]['audio']}\n\n"
+    else:
+        new_msg += f"***No phonetics found for {res_json[0]['word'].capitalize()}***\n\n"
     return new_msg
 
 # Function to get synonyms from the json object
@@ -48,28 +81,31 @@ def getPhonetics(res_json):
 def getSynonyms(res_json):
     new_msg = ""
     index = 1
-    for meaning in res_json[0]["meanings"]:
-        syns = []
-        for defns in meaning["definitions"]:
-            count = 0
-            if defns["synonyms"]:
-                for syn in defns["synonyms"]:
-                    syns.append(syn)
-                    count += 1
-                    if count == 2:
-                        break
-        if syns:
-            new_msg += \
-                f"{index}. ***Part of Speech*** : {meaning['partOfSpeech']}\n"
-            new_msg += f"    ***Synonyms*** : "
-            for syn in syns:
-                new_msg += syn + ", "
-            new_msg = new_msg[:-2] + "\n\n"
-            index += 1
+    pos = []
+    for res_j in res_json:
+        for meaning in res_j["meanings"]:
+            if meaning["partOfSpeech"] not in pos:
+                syns = []
+                for defns in meaning["definitions"]:
+                    count = 0
+                    if defns["synonyms"]:
+                        for syn in defns["synonyms"]:
+                            syns.append(syn)
+                            count += 1
+                            if count == 2:
+                                break
+                if syns:
+                    new_msg += \
+                        f"{index}. ***Part of Speech*** : {meaning['partOfSpeech']}\n"
+                    pos.append(meaning['partOfSpeech'])
+                    new_msg += f"    ***Synonyms*** : "
+                    for syn in syns:
+                        new_msg += syn + ", "
+                    new_msg = new_msg[:-2] + "\n\n"
+                    index += 1
 
     if index == 1:
         new_msg += f"***No synonyms found for {res_json[0]['word'].capitalize()}***\n\n"
-
     return new_msg
 
 # Function to get the origin from the json object
@@ -89,27 +125,45 @@ def getOrigin(res_json):
 def getAntonyms(res_json):
     index = 1
     new_msg = ""
-    for meaning in res_json[0]["meanings"]:
-        ants = []
-        for defns in meaning["definitions"]:
-            count = 0
-            if defns["antonyms"]:
-                for ant in defns["antonyms"]:
-                    ants.append(ant)
-                    count += 1
-                    if count == 2:
-                        break
-        if ants:
-            new_msg += \
-                f"{index}. ***Part of Speech*** : {meaning['partOfSpeech']}\n"
-            new_msg += f"    ***Antonyms*** : "
-            for ant in ants:
-                new_msg += ant + ", "
-            new_msg = new_msg[:-2] + "\n\n"
-            index += 1
+    pos = []
+    for res_j in res_json:
+        for meaning in res_j["meanings"]:
+            if meaning["partOfSpeech"] not in pos:
+                ants = []
+                for defns in meaning["definitions"]:
+                    count = 0
+                    if defns["antonyms"]:
+                        for ant in defns["antonyms"]:
+                            ants.append(ant)
+                            count += 1
+                            if count == 2:
+                                break
+                if ants:
+                    new_msg += \
+                        f"{index}. ***Part of Speech*** : {meaning['partOfSpeech']}\n"
+                    pos.append(meaning['partOfSpeech'])
+                    new_msg += f"    ***Antonyms*** : "
+                    for ant in ants:
+                        new_msg += ant + ", "
+                    new_msg = new_msg[:-2] + "\n\n"
+                    index += 1
 
     if index == 1:
         new_msg += f"***No antonyms found for {res_json[0]['word'].capitalize()}***\n\n"
+
+    return new_msg
+
+# Function to get meaning and examples from json object
+
+
+def getSlang(res_json):
+    new_msg = ""
+    if res_json["list"]:
+        defn = res_json["list"][0]
+        new_msg += \
+            f"***Definition*** : \n{defn['definition'].replace('[', '').replace(']', '')}\n"
+        replaceText = f"__*{defn['word']}*__"
+        new_msg += f"***Example*** : \n{defn['example'].replace('[', '').replace(']', '').replace(defn['word'], replaceText).replace(defn['word'].upper(), replaceText.upper()).replace(defn['word'].capitalize(), replaceText)}\n\n"
 
     return new_msg
 
@@ -140,6 +194,33 @@ def getData(func, data, msg):
         return msgs
 
 
+def getMenu():
+    return """
+
+**Dictionary Menu**
+            -*Pranav Agarwal*
+
+1. '***/mean*** <word1> <word2> ...' - Gives the __*part of speech, definition and examples*__.
+
+2. '***/org*** <word1> <word2> ...'  - Gives the __*origin*__ of the words.
+
+3. '***/phon*** <word1> <word2> ...' - Gives the __*phonetics text and audio*__.
+
+4. '***/syn*** <word1> <word2> ...'  - Gives the __*part of speech and the synonyms*__ under them.
+
+5. '***/ant*** <word1> <word2> ...'  - Gives the __*part of speech and the antonyms*__ under them.
+
+6. '***/dict*** <word1> <word2> ...' - Gives the __*short meaning, origin, phonetics, synonyms and antonyms*__.
+
+7. '***/slang*** <word1> <word2> ...' - Gives the __*meaning and example*__ for slangs.
+
+8. '***/menu***'\t\t\t\t\t\t\t\t\t- Gives the menu for the dictionary bot.
+
+---------
+
+"""
+
+
 @ client.event
 async def on_message(message):
     if message.author == client.user:
@@ -148,23 +229,23 @@ async def on_message(message):
     msg = message.content
 
     if msg.startswith("/mean"):
-        for m in getData(getMeaning, "Meaning", msg):
+        for m in getData(getLongMeaning, "Meaning", msg):
             await message.channel.send(m)
 
     if msg.startswith("/phon"):
-        for m in getData(getMeaning, "Meaning", msg):
+        for m in getData(getPhonetics, "Phonetics", msg):
             await message.channel.send(m)
 
     if msg.startswith("/syn"):
-        for m in getData(getMeaning, "Meaning", msg):
+        for m in getData(getSynonyms, "Synonyms", msg):
             await message.channel.send(m)
 
     if msg.startswith("/ant"):
-        for m in getData(getMeaning, "Meaning", msg):
+        for m in getData(getAntonyms, "Antonyms", msg):
             await message.channel.send(m)
 
     if msg.startswith("/org"):
-        for m in getData(getMeaning, "Meaning", msg):
+        for m in getData(getOrigin, "Origin", msg):
             await message.channel.send(m)
 
     if msg.startswith("/dict"):
@@ -181,7 +262,7 @@ async def on_message(message):
                     final_msg += f"***No results for {word}***\n\n"
                 else:
                     final_msg += "**Meaning**\n"
-                    final_msg += getMeaning(res_json)
+                    final_msg += getShortMeaning(res_json)
                     final_msg += "**Origin**\n"
                     final_msg += getOrigin(res_json)
                     final_msg += "**Phonetics**\n"
@@ -200,28 +281,35 @@ async def on_message(message):
             for m in msgs:
                 await message.channel.send(m)
 
-    if msg.startswith("/help"):
-        final_msg = """
+    if msg.startswith("/slang"):
+        l = msg.split()
+        if not l[1]:
+            return ["***Please enter atleast 1 word after '/slang'***"]
+        else:
+            msgs = []
+            for word in l[1:]:
+                res = requests.get(SLANG_API + word)
+                res_json = json.loads(res.text)
+                final_msg = f"**Word\t:\t{word.lower().capitalize()}**\n"
+                final_msg += getSlang(res_json) + "----------\n\n"
+                if not msgs:
+                    msgs.append(final_msg)
+                elif len(final_msg) + len(msgs[-1]) < 2000:
+                    msgs[-1] += final_msg
+                else:
+                    msgs.append(final_msg)
+            for m in msgs:
+                await message.channel.send(m)
 
-        **Dictionary Menu**
-                    -*Pranav Agarwal*
+    if msg.startswith("/menu"):
+        await message.channel.send(getMenu())
 
-        1. '***/mean*** <word1> <word2> ...' - Gives the __*part of speech, definition and examples*__.
 
-        2. '***/org*** <word1> <word2> ...'  - Gives the __*origin*__ of the words.
-
-        3. '***/phon*** <word1> <word2> ...' - Gives the __*phonetics text and audio*__.
-
-        4. '***/syn*** <word1> <word2> ...'  - Gives the __*part of speech and the synonyms*__ under them.
-
-        5. '***/ant*** <word1> <word2> ...'  - Gives the __*part of speech and the antonyms*__ under them.
-
-        6. '***/dict*** <word1> <word2> ...' - Gives the __*meaning, origin, phonetics, synonyms and antonyms*__.
-
-        ---------
-
-        """
-        await message.channel.send(final_msg)
+async def on_guild_join(guild):
+    for channel in guild.text_channels:
+        if channel.permissions_for(guild.me).send_messages:
+            await channel.send(getMenu())
+            break
 
 server()
 client.run(TOKEN)
